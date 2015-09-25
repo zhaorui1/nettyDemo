da59580b7d33cd3341dd8dd88e4e07a320c2bd6c
zhaorui <zhaorui@10.236.119.40>
2015-09-25 17:50:18 +0800

    first commit

---
+++ src/netty/server/DiscardServerHandler.java
@@ -0,0 +1,23 @@
+package netty.server;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Handles a server-side channel.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) {
+		ctx.write(msg);
+		ctx.flush();
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+		// Close the connection when an exception is raised.
+		cause.printStackTrace();
+		ctx.close();
+	}
+}
